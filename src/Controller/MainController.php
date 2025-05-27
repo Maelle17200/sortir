d04f9d6763e174d19c236baf6b4de70ec48cfa2d
Maelle Auzel <maelle.auzel2025@campus-eni.fr>
2025-05-27 22:33:16 +0200

    1. Supprime HTML "inutile" 2. Configuration security.yaml 3. Modification base.html.twig + css 4. Création login

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -15,4 +15,10 @@ class MainController extends AbstractController
         return $this->render('main/accueil.html.twig');
     }
 
+    #[Route('/login', name: 'main_login', methods: ['GET'])]
+    public function login():Response
+    {
+        return $this->render('security/login.html.twig');
+    }
+
 }
--- src/DataFixtures/ParticipantFixtures.php
+++ src/DataFixtures/ParticipantFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Participant;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,8 +11,6 @@ class ParticipantFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         // $product = new Product();
-        // $manager->persist($product);
-
-        $manager->flush();
+        // $manager->flush();
     }
 }
