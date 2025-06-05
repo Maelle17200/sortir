2c1b7c73c0611543b679c28cf0d925b9ea9ced3e
perrine2991 <perrine.albericci2025@campus-eni.fr>
2025-06-05 21:56:52 +0200

    Page administrateur

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -13,9 +13,17 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/admin')]
-class RegistrationController extends AbstractController
+class AdminController extends AbstractController
 {
-    #[Route('/register', name: 'app_register', methods: ['GET','POST'])]
+    #[Route('/accueil', name: 'admin_accueil', methods: ['GET','POST'])]
+    public function accueil(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager): Response
+    {
+        return $this->render('admin/admin.html.twig', [
+
+        ]);
+    }
+
+    #[Route('/register', name: 'admin_register', methods: ['GET','POST'])]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager): Response
     {
         $user = new Participant();
@@ -31,7 +39,7 @@ class RegistrationController extends AbstractController
 
             if ($user->getCampus() === null) {
                 $this->addFlash('error', 'Veuillez sélectionner un campus.');
-                return $this->redirectToRoute('app_register');
+                return $this->redirectToRoute('admin_register');
             }
 
             $entityManager->persist($user);
@@ -42,7 +50,7 @@ class RegistrationController extends AbstractController
             return $security->login($user, 'form_login', 'main');
         }
 
-        return $this->render('registration/register.html.twig', [
+        return $this->render('admin/register.html.twig', [
             'registrationForm' => $form,
         ]);
     }
